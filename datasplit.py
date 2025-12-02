6409f4e45021bb67cfbd3059d01a58985c63df7a
KevCoder007 <kstratton8376@sdsu.edu>
2025-12-01 19:45:21 -0800

    Datasplit

---
+++ datasplit.py
@@ -0,0 +1,14 @@
+# split data into training, and test sets
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv("CleanedUpData.csv")
+
+train_data, test_data = train_test_split(data, 
+                test_size=0.2, # 20% for test set, 80% for training + validation
+                random_state=42, #ensure reproducibility
+                shuffle=True)
+
+train_data.to_csv("train_data.csv", index=False)
+test_data.to_csv("test_data.csv", index=False)
+
